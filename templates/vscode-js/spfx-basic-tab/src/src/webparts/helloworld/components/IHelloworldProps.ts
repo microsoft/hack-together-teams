db96bcaf04c0a3fdde5aa1b863f6f0784c2371fd
kamal shree <kamalshree2784@gmail.com>
2023-05-17 14:33:40 +0530

    Uploaded the spfx basic tab Template
    Uploaded the spfx basic tab Template

---
+++ templates/vscode-js/spfx-basic-tab/src/src/webparts/helloworld/components/IHelloworldProps.ts
@@ -0,0 +1,7 @@
+export interface IHelloworldProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ templates/vscode-js/spfx-basic-tab/src/src/webparts/helloworld/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IHelloworldWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'HelloworldWebPartStrings' {
+  const strings: IHelloworldWebPartStrings;
+  export = strings;
+}
